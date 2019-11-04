eed310cd7efb8f7bc1dd72b622e9e58c2fa1d064
William Coe <willcoe@aiven.io>
2019-11-04 12:37:48 +1100

    postgres: add node.js example

---
+++ postgresql/nodejs/README.md
@@ -0,0 +1,14 @@
+### PostgreSQL node.js Example
+
+This example uses the [pq](https://www.npmjs.com/package/pg) library to connect to PostgreSQL and perform a simple query.
+#### Installing Dependencies  
+
+```
+npm install
+```
+
+#### Running The Example
+Note: You can retrieve the connection information from the Aiven Console overview tab.
+```
+node index.js --host <host> --port <port> --password <password>
+```
---
+++ postgresql/nodejs/index.js
@@ -0,0 +1,29 @@
+const yargs = require('yargs');
+const example = require('./example');
+
+const argv = yargs
+    .option('host', {
+        description: 'Kafka host in the form host:port',
+        type: 'string',
+        required: true
+    }).option('host', {
+        description: 'Postgres host',
+        type: 'string',
+        required: true
+    }).option('port', {
+        description: 'Postgres port',
+        type: 'string',
+        required: true
+    }).option('password', {
+        description: 'Postgres password',
+        type: 'string',
+        required: true
+    }).option('user', {
+        description: 'Postgres user',
+        type: 'string',
+        default: 'avnadmin'
+    }).number("port")
+    .argv;
+
+example.postgresExample(argv.host, argv.password, argv.port, argv.user);
+
