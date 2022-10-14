e9cf3ebce718032cffba39abc62f8d5cb46e917b
Maulik Parikh <maulik.parikh@aiven.io>
2022-10-14 16:43:04 -0300

    Updating the script to build hostname for the fork automatically and not supplying as an argument

--- solutions/scripts/avn-fork-mysqldump.sh
+++ solutions/scripts/avn-fork-mysqldump.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 usage() {
-    printf "avn-mysqldump create a fork of mysql to take a mysqldump then terminates the fork.\nUsage: ./avn-fork-mysqldump.sh mysqlservice hostname port username password aiven_project mysql_fork_name\n" 1>&2;
+    printf "avn-mysqldump create a fork of mysql to take a mysqldump then terminates the fork.\nUsage: ./avn-fork-mysqldump.sh mysqlservice port username password aiven_project mysql_fork_name\n" 1>&2;
     exit 1;
 }
 
-if [ "$#" -ne 7 ]; then
+if [ "$#" -ne 6 ]; then
     usage;
 fi
 
 AVN_SERVICE=$1
-AVN_HOST=$2
-AVN_PORT=$3
-AVN_USER=$4
-AVN_PASS=$5
-AVN_PROJECT=$6
+AVN_PORT=$2
+AVN_USER=$3
+AVN_PASS=$4
+AVN_PROJECT=$5
 AVN_SERVICE_PLAN=startup-4
-AVN_MYSQL_FORK_NAME=$7
+AVN_MYSQL_FORK_NAME=$6
+AVN_FORK_HOST=$AVN_MYSQL_FORK_NAME-$AVN_PROJECT.aivencloud.com
 
 avn service create $AVN_MYSQL_FORK_NAME -t mysql --plan $AVN_SERVICE_PLAN --project $AVN_PROJECT -c service_to_fork_from=$AVN_SERVICE
 
@@ -25,15 +25,15 @@ if [ "$?" -ne 0 ]; then
     exit 1;
 fi
 avn service wait $AVN_MYSQL_FORK_NAME
-sleep 60
+sleep 180
 
 # do not backup default system databases - mysql, performance_schema, information_schema, sys
-AVN_DBS=$(echo "show databases" | mysql --host=$AVN_HOST --port=$AVN_PORT -u $AVN_USER --password=$AVN_PASS | grep -Ev "^(Database|mysql|performance_schema|information_schema|sys)$")
+AVN_DBS=$(echo "show databases" | mysql --host=$AVN_FORK_HOST --port=$AVN_PORT -u $AVN_USER --password=$AVN_PASS | grep -Ev "^(Database|mysql|performance_schema|information_schema|sys)$")
 
 AVN_BACKUP=$AVN_MYSQL_FORK_NAME-backup-$(date +'%Y%m%d%H%M%S').gz
 echo "Backup databases: ["$AVN_DBS"]" to $AVN_BACKUP
 
 ## Compressed version
-mysqldump --host=$AVN_HOST --port=$AVN_PORT -u $4 -p --password=$5 -B $AVN_DBS | gzip -9 > $AVN_BACKUP
+mysqldump --host=$AVN_FORK_HOST --port=$AVN_PORT -u $4 -p --password=$5 -B $AVN_DBS | gzip -9 > $AVN_BACKUP
 
 echo $AVN_AVN_MYSQL_FORK_NAME | avn service terminate --project $AVN_PROJECT -f $AVN_MYSQL_FORK_NAME
