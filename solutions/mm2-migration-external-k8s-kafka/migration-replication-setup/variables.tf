a15a9803f672f3f442c8d2a53c9f8cb57701adac
Maulik Parikh <maulik.parikh@aiven.io>
2024-06-12 19:14:54 -0300

    update - example to provision external service integrations and replication flows

--- solutions/mm2-migration-external-k8s-kafka/migration-replication-setup/variables.tf
+++ solutions/mm2-migration-external-k8s-kafka/migration-replication-setup/variables.tf
@@ -10,26 +10,24 @@ variable aiven_project_name {
 /*variable "source_kafka_service" {
   description = "Aiven Source Kafka Service"
   type        = string
-  default = "hv-ingest-test"
+  default = "source-aiven-kafka-service-name"
 }*/
 
 variable "external_source_strimzi_kafka_endpoint_id" {
   description = "Endpoint ID - Strimzi Source Kafka Service via External Integration"
   type        = string
-  //default = "mparikh-demo/c725dd4a-23d4-435a-baa2-e5a614aee170"
   //default = "your-aiven-project-name/source-integration-endpoint-id"
 }
 
 variable "destination_kafka_service" {
   description = "Strimzi Source Kafka Service"
   type        = string
-  //default = "kafka-oci-ashburn"
+  //default = "destination-aiven-kafka-service-name"
 }
 
 variable "external_aiven_destination_kafka_service_1_endpoint_id" {
   description = "Endpoint ID - Aiven Kafka Destination Service via External Integration"
   type        = string
-  //default = "mparikh-demo/c725dd4a-23d4-435a-baa2-e5a614aee170"
   //default = "your-aiven-project-name/source-integration-endpoint-id"
 }
 
