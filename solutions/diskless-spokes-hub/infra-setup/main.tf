3de87e28b497372f934d383b154235e3d57418d4
Maulik Parikh <maulik.parikh@aiven.io>
2026-03-26 10:51:15 -0300

    Added MM2 for Data Replication and Service Integration for those

--- solutions/diskless-spokes-hub/infra-setup/main.tf
+++ solutions/diskless-spokes-hub/infra-setup/main.tf
@@ -1,4 +1,4 @@
-# create Kafka
+# create Hub Kafka
 resource "aiven_kafka" "hub_kafka" {
   project                 = var.aiven_project_name
   cloud_name              = var.cloud_name_hub
@@ -39,6 +39,7 @@ resource "aiven_kafka" "hub_kafka" {
   }
 }
 
+# create spoke kafka
 resource "aiven_kafka" "spoke_kafka_1" {
   project                 = var.aiven_project_name
   cloud_name              = var.cloud_name_spoke_1
@@ -85,7 +86,7 @@ resource "aiven_kafka_mirrormaker" "mm2_schema_replication_1" {
   project      = var.aiven_project_name
   cloud_name   = var.cloud_name_hub
   plan         = var.mm2_plan_hub_cluster
-  service_name = "${var.service_prefix}-mm2-spoke-1"
+  service_name = "${var.service_prefix}-mm2-spoke-1-schema-replication"
 
   kafka_mirrormaker_user_config {
     kafka_mirrormaker {
@@ -104,19 +105,44 @@ resource "aiven_kafka_mirrormaker" "mm2_schema_replication_1" {
   }
 }
 
+#create MM2 for Spoke to Hub Schema Replication
+resource "aiven_kafka_mirrormaker" "mm2_data_replication_spoke_1" {
+  project      = var.aiven_project_name
+  cloud_name   = var.cloud_name_hub
+  plan         = var.mm2_plan_spoke_cluster
+  service_name = "${var.service_prefix}-mm2-data-replication-spoke-1"
+
+  kafka_mirrormaker_user_config {
+    kafka_mirrormaker {
+      refresh_groups_enabled = true
+      refresh_groups_interval_seconds = 10 //changed from 10 to 180
+      refresh_topics_enabled          = false // just sync schema topics
+      refresh_topics_interval_seconds = 60
+      sync_group_offsets_enabled = true
+      sync_group_offsets_interval_seconds = 10
+      sync_topic_configs_enabled = false // just sync schema topics
+      tasks_max_per_cpu = 2 //changed from 2 to 4
+      emit_checkpoints_enabled = true
+      emit_checkpoints_interval_seconds = 10
+      offset_lag_max = 1
+    }
+  }
+}
+
 
 #create wait
 resource "time_sleep" "wait_mm2_readiness" {
   depends_on = [
     aiven_kafka.hub_kafka,
     aiven_kafka.spoke_kafka_1,
-    aiven_kafka_mirrormaker.mm2_schema_replication_1
+    aiven_kafka_mirrormaker.mm2_schema_replication_1,
+    aiven_kafka_mirrormaker.mm2_data_replication_spoke_1
   ]
   create_duration = "600s"
 }
 
-# // MM2 Hub service integration
-resource "aiven_service_integration" "mm2-hub-svc-integration" {
+# // MM2 Hub service integration for Schema Replication
+resource "aiven_service_integration" "mm2-hub-svc-integration-schema-source" {
 project                  = var.aiven_project_name
 integration_type         = "kafka_mirrormaker"
 source_service_name      = aiven_kafka.hub_kafka.service_name
@@ -132,14 +158,48 @@ kafka_mirrormaker_user_config {
     }
 }
 
-# // MM2 spoke service integration
-resource "aiven_service_integration" "mm2-spoke-svc-integration" {
+# // MM2 spoke service integration for Schema Replication
+resource "aiven_service_integration" "mm2-spoke-svc-integration-schema-destination" {
 project                  = var.aiven_project_name
 integration_type         = "kafka_mirrormaker"
 source_service_name      = aiven_kafka.spoke_kafka_1.service_name
 destination_service_name = aiven_kafka_mirrormaker.mm2_schema_replication_1.service_name
 kafka_mirrormaker_user_config {
     cluster_alias = "aiven-spoke-schema-receiver-cluster"
+    kafka_mirrormaker {
+        producer_max_request_size = 66901452 // default
+        producer_buffer_memory = 33554432 // default, required size is that producer batch fits to buffer memory.
+        producer_batch_size = 12000 
+        producer_linger_ms = 100
+    }
+    }
+}
+
+# // MM2 Hub service integration for Data Replication
+resource "aiven_service_integration" "mm2-hub-svc-integration-data-destination" {
+project                  = var.aiven_project_name
+integration_type         = "kafka_mirrormaker"
+source_service_name      = aiven_kafka.hub_kafka.service_name
+destination_service_name = aiven_kafka_mirrormaker.mm2_data_replication_spoke_1.service_name
+kafka_mirrormaker_user_config {
+    cluster_alias = "aiven-hub-data-destination-cluster" // could suffix with region name
+    kafka_mirrormaker {
+        producer_max_request_size = 66901452 // default
+        producer_buffer_memory = 33554432 // default, required size is that producer batch fits to buffer memory.
+        producer_batch_size = 12000
+        producer_linger_ms = 100
+    }
+    }
+}
+
+# // MM2 spoke service integration for Data Replication
+resource "aiven_service_integration" "mm2-spoke-svc-integration-data-source-1" {
+project                  = var.aiven_project_name
+integration_type         = "kafka_mirrormaker"
+source_service_name      = aiven_kafka.spoke_kafka_1.service_name
+destination_service_name = aiven_kafka_mirrormaker.mm2_data_replication_spoke_1.service_name
+kafka_mirrormaker_user_config {
+    cluster_alias = "aiven-spoke-data-source-cluster-1" //could use name suffix with region name 
     kafka_mirrormaker {
         producer_max_request_size = 66901452 // default
         producer_buffer_memory = 33554432 // default, required size is that producer batch fits to buffer memory.
